c581bd0d5a0843aef99f891b9a4f49006867cf79
prnvvv <prannavakhanth12@gmail.com>
2024-06-29 21:05:21 +0530

    Update wikipedia_webscraping.py

--- wikipedia_webscraping.py
+++ wikipedia_webscraping.py
@@ -15,6 +15,7 @@ soup = BeautifulSoup(response.text, "lxml")
 
 GainerCompanyNames = soup.find_all("div", class_ = "ZvmM7")
 GainerMarketValue = soup.find_all("div", class_ = "YMlKec")
+GainerPercentage = soup.find_all("span", class_ = "V53LMb")
 
 GainerCompanyNames_List = []
 GainerMarketValue_List = []
@@ -25,7 +26,10 @@ for name in GainerCompanyNames:
 for money in GainerMarketValue[10: 10+len(GainerCompanyNames_List)]:
     GainerMarketValue_List.append(money.text)
 
+for percentage in GainerPercentage:
+    print(percentage.text)
 
-df = pd.DataFrame({"Gainer Company Names": GainerCompanyNames_List, "Market Value": GainerMarketValue_List}, index = np.arange(1, len(GainerCompanyNames_List)+1))
 
-print(df)
+Gainers_DataFrame = pd.DataFrame({"Gainer Company Names": GainerCompanyNames_List, "Market Value": GainerMarketValue_List}, index = np.arange(1, len(GainerCompanyNames_List)+1))
+
+
