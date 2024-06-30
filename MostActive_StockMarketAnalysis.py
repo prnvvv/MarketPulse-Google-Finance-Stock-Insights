b28dd088bbf4fc938486f2f64a9577d43f8f0faf
prnvvv <prannavakhanth12@gmail.com>
2024-06-30 18:26:54 +0530

    Update MostActive_StockMarketAnalysis.py

---
+++ MostActive_StockMarketAnalysis.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+URL = f"https://www.google.com/finance/markets/most-active"
+
+try:
+    response = requests.get(URL)
+    response.raise_for_status()
+except requests.exceptions.RequestException as e:
+    print(f"Error fetching data from {URL}.")
+
+soup = BeautifulSoup(response.text, "lxml")
+
+MostActiveCompanyNames = soup.find_all("div", class_ = "ZvmM7")
+MostActiveMarketValue = soup.find_all("div", class_ = "YMlKec")
+MostActivePercentage = soup.find_all("span", class_ = "NydbP VOXKNe")
+
+MostActiveCompanyNames_List = []
+MostActiveMarketValue_List = []
+MostActivePercentage_List = []
+
+for name in MostActiveCompanyNames:
+    MostActiveCompanyNames_List.append(name.text)
+
+for money in MostActiveMarketValue[10: 10+len(MostActiveCompanyNames_List)]:
+    MostActiveMarketValue_List.append(money.text)
+
+for percentage in MostActivePercentage[3: len(MostActiveCompanyNames_List)]:
+    ActualPercentage = percentage["aria-label"]
+    print(ActualPercentage)
+
+#MostActive_DataFrame = pd.DataFrame({"Gainer Company Names": MostActiveCompanyNames_List, "Market Value": MostActiveMarketValue_List, "Increase Percentage": MostActivePercentage_List}, index = np.arange(1, len(MostActiveCompanyNames_List)+1))
+
+
