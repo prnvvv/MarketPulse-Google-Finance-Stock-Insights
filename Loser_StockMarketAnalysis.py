452577b39d97c3ff002642a4a3c2a0c74ed56473
prnvvv <prannavakhanth12@gmail.com>
2024-06-30 17:51:48 +0530

    Update Loser_StockMarketAnalysis.py

--- Loser_StockMarketAnalysis.py
+++ Loser_StockMarketAnalysis.py
@@ -3,19 +3,35 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-URL = f"https://www.google.com/finance/markets/losers"
+URL = f"https://www.google.com/finance/markets/gainers"
 
 try:
     response = requests.get(URL)
     response.raise_for_status()
-except requests.RequestException as e:
-    print(f"Error while fetching data from {URL}.")
+except requests.exceptions.RequestException as e:
+    print(f"Error fetching data from {URL}.")
 
 soup = BeautifulSoup(response.text, "lxml")
 
-CompanyNames = soup.find_all("div", class_ = "ZvmM7")
+LoserCompanyNames = soup.find_all("div", class_ = "ZvmM7")
+LoserMarketValue = soup.find_all("div", class_ = "YMlKec")
+LoserPercentage = soup.find_all("div", class_ = "JwB6zf")
 
-for name in CompanyNames:
-    print(name.text)
+LoserCompanyNames_List = []
+LoserMarketValue_List = []
+LoserPercentage_List = []
 
+for name in LoserCompanyNames:
+    LoserCompanyNames_List.append(name.text)
+
+for money in LoserMarketValue[10: 10+len(LoserCompanyNames_List)]:
+    LoserMarketValue_List.append(money.text)
+
+for percentage in LoserPercentage[10: 10+len(LoserCompanyNames_List)]:
+    LoserPercentage_List.append(percentage.text)
+
+
+Losers_DataFrame = pd.DataFrame({"Gainer Company Names": LoserCompanyNames_List, "Market Value": LoserMarketValue_List, "Increase Percentage": LoserPercentage_List}, index = np.arange(1, len(LoserCompanyNames_List)+1))
+
+print(Gainers_DataFrame)
 
