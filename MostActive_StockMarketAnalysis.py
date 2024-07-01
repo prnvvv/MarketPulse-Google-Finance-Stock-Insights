619f52628e02b6b2bd0d9dd01cf95e8fb630f219
prnvvv <prannavakhanth12@gmail.com>
2024-07-01 20:34:43 +0530

    Update MostActive_StockMarketAnalysis.py

--- MostActive_StockMarketAnalysis.py
+++ MostActive_StockMarketAnalysis.py
@@ -15,11 +15,13 @@ soup = BeautifulSoup(response.text, "lxml")
 
 MostActiveCompanyNames = soup.find_all("div", class_ = "ZvmM7")
 MostActiveMarketValue = soup.find_all("div", class_ = "YMlKec")
+MostActiveCompanyAbbr = soup.find_all("div", class_ = "COaKTb")
 
 MostActiveCompanyNames_List = []
 MostActiveMarketValue_List = []
 MostActivePercentage_List = []
 MostActiveChangeValue_List = []
+MostActiveCompanyAbbr_List = []
 
 for name in MostActiveCompanyNames:
     MostActiveCompanyNames_List.append(name.text)
@@ -27,6 +29,15 @@ for name in MostActiveCompanyNames:
 for money in MostActiveMarketValue[10: 10+len(MostActiveCompanyNames_List)]:
     MostActiveMarketValue_List.append(money.text)
 
+for abbrnName in MostActiveCompanyAbbr[:len(MostActiveCompanyNames_List)]:
+    MostActiveCompanyAbbr_List.append(abbrnName.text)
+
 MostActive_DataFrame = pd.DataFrame({"Most Active Company Names": MostActiveCompanyNames_List, "Market Value": MostActiveMarketValue_List}, index = np.arange(1, len(MostActiveCompanyNames_List)+1))
 
 print(MostActive_DataFrame)
+
+for i in MostActiveCompanyAbbr_List:
+    print(i.upper())
+    NextPageURL = f"https://www.google.com/finance/quote/{i.upper}:NSE"
+
+
