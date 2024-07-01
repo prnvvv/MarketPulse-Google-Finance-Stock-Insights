5aaab38fb8945fc79ee91ed1ab5d7096ac540047
prnvvv <prannavakhanth12@gmail.com>
2024-07-01 20:42:08 +0530

    Update Loser_StockMarketAnalysis.py

--- Loser_StockMarketAnalysis.py
+++ Loser_StockMarketAnalysis.py
@@ -9,7 +9,7 @@ try:
     response = requests.get(URL)
     response.raise_for_status()
 except requests.exceptions.RequestException as e:
-    print(f"Error fetching data from {URL}.")
+    print(f"Error fetching data from {URL}: {e}")
 
 soup = BeautifulSoup(response.text, "lxml")
 
