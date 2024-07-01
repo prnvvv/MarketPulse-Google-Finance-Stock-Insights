51b22b3e152a0cd9d0f6ab40b47d486fc2ba9310
prnvvv <prannavakhanth12@gmail.com>
2024-07-01 20:05:50 +0530

    Update MostActive_StockMarketAnalysis.py

--- MostActive_StockMarketAnalysis.py
+++ MostActive_StockMarketAnalysis.py
@@ -15,7 +15,7 @@ soup = BeautifulSoup(response.text, "lxml")
 
 MostActiveCompanyNames = soup.find_all("div", class_ = "ZvmM7")
 MostActiveMarketValue = soup.find_all("div", class_ = "YMlKec")
-MostActivePercentage = soup.find_all("path")
+
 
 MostActiveCompanyNames_List = []
 MostActiveMarketValue_List = []
@@ -28,16 +28,10 @@ for name in MostActiveCompanyNames:
 for money in MostActiveMarketValue[10: 10+len(MostActiveCompanyNames_List)]:
     MostActiveMarketValue_List.append(money.text)
 
-for i in MostActivePercentage:
-    if i["d"]:
-        if i["d"] == "M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z":
-            ChangeValue = "Down By "
-            MostActiveChangeValue_List.append(ChangeValue)
-        elif i["d"] == "M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z":
-            ChangeValue = "Up by "
-            MostActiveChangeValue_List.append(ChangeValue)
-    else:
-        MostActiveChangeValue_List.append("No Change")
+ChangeValue = ["NydbP nZQ6l","NydbP VOXKNe"]
+
+MostActivePercentage = soup.find_all("span", class_ = "xVyTdb NN5r3b ")
+
 
 #MostActive_DataFrame = pd.DataFrame({"Gainer Company Names": MostActiveCompanyNames_List, "Market Value": MostActiveMarketValue_List, "Increase Percentage": MostActivePercentage_List}, index = np.arange(1, len(MostActiveCompanyNames_List)+1))
 
