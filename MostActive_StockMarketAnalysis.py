42e61dd2df522ce3c3c05dc51958d25da64c9ecd
prnvvv <prannavakhanth12@gmail.com>
2024-07-01 20:46:41 +0530

    Update MostActive_StockMarketAnalysis.py

--- MostActive_StockMarketAnalysis.py
+++ MostActive_StockMarketAnalysis.py
@@ -36,19 +36,19 @@ MostActive_DataFrame = pd.DataFrame({"Most Active Company Names": MostActiveComp
 
 #print(MostActive_DataFrame)
 
-for i in MostActiveCompanyAbbr_List:
-    NextPageURL = f"https://www.google.com/finance/quote/{i.upper()}:NSE"
-    try:
-        response = requests.get(NextPageURL)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        print(f"Error while fetching data fro {NextPageURL}.")
 
-    soup2 = BeautifulSoup(response.text, "lxml")
 
-    ChangeValue = soup2.find_all("span", class_ = "P2Luy Ebnabc ZYVHBb")
+NextPageURL = f"https://www.google.com/finance/quote/IDEA:NSE"
+try:
+    response = requests.get(NextPageURL)
+    response.raise_for_status()
+except requests.exceptions.RequestException as e:
+    print(f"Error while fetching data fro {NextPageURL}.")
 
-    for j in ChangeValue:
-        print(ChangeValue.text)
+soup2 = BeautifulSoup(response.text, "lxml")
+   
+ChangeValue = soup2.find_all("span", class_ = "P2Luy Ebnabc ZYVHBb")
 
+for j in ChangeValue:
+    print(ChangeValue.text)
 
