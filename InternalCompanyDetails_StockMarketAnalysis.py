a53d867ab24e99f98d4c4d227ab9bed94f3362a2
prnvvv <prannavakhanth12@gmail.com>
2024-07-01 21:14:30 +0530

    Update InternalCompanyDetails_StockMarketAnalysis.py

--- InternalCompanyDetails_StockMarketAnalysis.py
+++ InternalCompanyDetails_StockMarketAnalysis.py
@@ -10,6 +10,6 @@ except requests.exceptions.RequestException as e:
 
 soup2 = BeautifulSoup(response.text, "lxml")
    
-heading = soup2.find_all ("span", class_ = "P2Luy Ez2Ioe ZYVHBb")
+heading = soup2.find("div", class_ = "enJeMd")
 
-print(heading)
+print(heading) 
