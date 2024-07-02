bea4a2d66666bd92b9535406595311b7b6b74d36
prnvvv <prannavakhanth12@gmail.com>
2024-07-02 21:08:56 +0530

    Update FamousCompany_StockMarketAnalysis.py

--- FamousCompany_StockMarketAnalysis.py
+++ FamousCompany_StockMarketAnalysis.py
@@ -15,3 +15,16 @@ soup = BeautifulSoup(response.text, "lxml")
 
 FamousCompanyNames = soup.find_all("div", class_ = "ZvmM7")
 FamousMarketValues = soup.find_all("div", class_ = "YMlKec")
+FamousPercentages = soup.find_all("div", class_ = "JwB6zf")
+FamousChangeValues = soup.find_all("path")
+
+FamousCompanyNames_List = []
+FamousChangeValues_List = []
+FamousMarketValues_List = []
+FamousPercentages_List = []
+
+for colour in FamousChangeValues:
+    if colour["d"] ==  "M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z":
+        FamousChangeValues_List.append("Down By ")
+    elif colour["d"] == "M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z":
+        FamousChangeValues_List.append("Up By ")
