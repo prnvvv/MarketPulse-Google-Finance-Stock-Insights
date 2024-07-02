42ed6f69841036ee7f4a1a92eae79ef9ee4e3a1e
prnvvv <prannavakhanth12@gmail.com>
2024-07-02 20:34:15 +0530

    Update MostActive_StockMarketAnalysis.py

--- MostActive_StockMarketAnalysis.py
+++ MostActive_StockMarketAnalysis.py
@@ -15,7 +15,7 @@ soup = BeautifulSoup(response.text, "lxml")
 
 MostActiveCompanyNames = soup.find_all("div", class_ = "ZvmM7")
 MostActiveMarketValue = soup.find_all("div", class_ = "YMlKec")
-MostActiveCompanyAbbr = soup.find_all("div", class_ = "COaKTb")
+MostActiveCompany = soup.find_all("path")
 
 MostActiveCompanyNames_List = []
 MostActiveMarketValue_List = []
@@ -29,26 +29,18 @@ for name in MostActiveCompanyNames:
 for money in MostActiveMarketValue[10: 10+len(MostActiveCompanyNames_List)]:
     MostActiveMarketValue_List.append(money.text)
 
-for abbrnName in MostActiveCompanyAbbr[:len(MostActiveCompanyNames_List)]:
-    MostActiveCompanyAbbr_List.append(abbrnName.text)
+for colour in MostActiveCompany:
+    if colour["d"] ==  "M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z":
+        MostActiveCompanyAbbr_List.append("Down By ")
+    elif colour["d"] == "M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z":
+        MostActiveCompanyAbbr_List.append("Up By ")
 
 MostActive_DataFrame = pd.DataFrame({"Most Active Company Names": MostActiveCompanyNames_List, "Market Value": MostActiveMarketValue_List}, index = np.arange(1, len(MostActiveCompanyNames_List)+1))
 
 #print(MostActive_DataFrame)
 
+print(len(MostActiveCompanyNames_List))
 
+print(MostActiveCompanyAbbr_List[10: 10 + len(MostActiveCompanyNames_List)])
 
-NextPageURL = f"https://www.google.com/finance/quote/IDEA:NSE"
-try:
-    response = requests.get(NextPageURL)
-    response.raise_for_status()
-except requests.exceptions.RequestException as e:
-    print(f"Error while fetching data fro {NextPageURL}.")
-
-soup2 = BeautifulSoup(response.text, "lxml")
-   
-ChangeValue = soup2.find_all("span", class_ = "P2Luy Ebnabc ZYVHBb")
-
-for j in ChangeValue:
-    print(ChangeValue.text)
 
