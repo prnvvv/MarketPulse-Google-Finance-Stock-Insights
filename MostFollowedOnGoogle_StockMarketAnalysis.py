15a333012e3fb0bb0772c131394bb51d838f0ccf
prnvvv <prannavakhanth12@gmail.com>
2024-07-03 21:56:52 +0530

    Update MostFollowedOnGoogle_StockMarketAnalysis.py

--- MostFollowedOnGoogle_StockMarketAnalysis.py
+++ MostFollowedOnGoogle_StockMarketAnalysis.py
@@ -15,14 +15,22 @@ soup = BeautifulSoup(response.text, "lxml")
 
 MFGCompanyNames = soup.find_all("div", class_ = "TwnKPb")
 MFGPercentages = soup.find_all("div", class_ = "JwB6zf")
+MFGChangeValues = soup.find_all("div", class_ = "path")
 
 MFGCompanyNames_List = []
 MFGPercentages_List = []
+MFGChangeValues_List = []
 
 for name in MFGCompanyNames:
     MFGCompanyNames_List.append(name.text)
 
-for percentage in MFGPercentages:
+for percentage in MFGPercentages[16: 16 + len(MFGCompanyNames_List)]:
     MFGPercentages_List.append(percentage.text)
 
-print(MFGPercentages_List)
+for colour in MFGChangeValues:
+    if colour["d"] ==  "M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z":
+        MFGChangeValues_List.append("Down By ")
+    elif colour["d"] == "M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z":
+        MFGChangeValues_List.append("Up By ")
+
+print(MFGChangeValues_List)
