4d9dee85ea17a82f490a3ca3864e1a80a1d33640
prnvvv <prannavakhanth12@gmail.com>
2024-07-05 15:09:13 +0530

    Update IndividualComapnyAnalysis_StockMarketAnalysis.py

--- IndividualComapnyAnalysis_StockMarketAnalysis.py
+++ IndividualComapnyAnalysis_StockMarketAnalysis.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 def IndividualCompanyAnalysis():
     code = input("Enter the Company code: ")
 
-    URL = f"https://www.google.com/finance/quote/{code}:NSE"
+    URL = f"https://www.google.com/finance/quote/{code.upper()}:NSE"
 
     try:
         response = requests.get(URL)
@@ -23,12 +23,14 @@ def IndividualCompanyAnalysis():
 
     CompanyPercentage_List = []
 
-    print(Companyname)
-    print(CompanyMarketValue)
-
     for percentage in CompanyPercentage:
         CompanyPercentage_List.append(percentage.text)
     CompanyPercentage = CompanyPercentage_List[19]
 
-    print(CompanyPercentage)
+    print()
     print(Date)
+    print()
+
+    print(pd.Series([Companyname, CompanyMarketValue, CompanyPercentage], index = ["Company Name", "Market Value", "Percentage"]))
+
+IndividualCompanyAnalysis()
