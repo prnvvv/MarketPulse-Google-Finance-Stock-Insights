d04da393cfc52d8f714a3d8b68b6506c374e9343
prnvvv <prannavakhanth12@gmail.com>
2024-07-05 14:54:36 +0530

    Update Gainers_StockMarketAnalysis.py

--- Gainers_StockMarketAnalysis.py
+++ Gainers_StockMarketAnalysis.py
@@ -14,24 +14,29 @@ except requests.exceptions.RequestException as e:
 soup = BeautifulSoup(response.text, "lxml")
 
 GainerCompanyNames = soup.find_all("div", class_ = "ZvmM7")
-GainerMarketValue = soup.find_all("div", class_ = "YMlKec")
-GainerPercentage = soup.find_all("div", class_ = "JwB6zf")
+GainerMarketValues = soup.find_all("div", class_ = "YMlKec")
+GainerPercentages = soup.find_all("div", class_ = "JwB6zf")
+GainerCompanyCode = soup.find_all("div", class_ = "COaKTb")
 
 GainerCompanyNames_List = []
-GainerMarketValue_List = []
-GainerPercentage_List = []
+GainerMarketValues_List = []
+GainerPercentages_List = []
+GainerCompanyCode_List = []
 
 for name in GainerCompanyNames:
     GainerCompanyNames_List.append(name.text)
 
-for money in GainerMarketValue[10: 10+len(GainerCompanyNames_List)]:
-    GainerMarketValue_List.append(money.text)
+for money in GainerMarketValues[10: 10+len(GainerCompanyNames_List)]:
+    GainerMarketValues_List.append(money.text)
 
-for percentage in GainerPercentage[10: 10+len(GainerCompanyNames_List)]:
-    GainerPercentage_List.append(percentage.text)
+for percentage in GainerPercentages[10: 10+len(GainerCompanyNames_List)]:
+    GainerPercentages_List.append(percentage.text)
+
+for codename in GainerCompanyCode:
+    GainerCompanyCode_List.append(codename.text)
 
 
-Gainers_DataFrame = pd.DataFrame({"Gainer Company Names": GainerCompanyNames_List, "Market Value": GainerMarketValue_List, "Increase Percentage": GainerPercentage_List}, index = np.arange(1, len(GainerCompanyNames_List)+1))
+Gainers_DataFrame = pd.DataFrame({"Gainer Company Names": GainerCompanyNames_List, "Market Value": GainerMarketValue_List, "Increase Percentage": GainerPercentages_List}, index = np.arange(1, len(GainerCompanyNames_List)+1))
 
-print(Gainers_DataFrame)
+#print(Gainers_DataFrame)
 
