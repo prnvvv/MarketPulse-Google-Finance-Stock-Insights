6ec6db82ee0bfb5bd17dd9ef88f7acca8c6d2134
prnvvv <prannavakhanth12@gmail.com>
2024-07-06 19:14:15 +0530

    Update Losers_StockMarketAnalysis.py

--- Losers_StockMarketAnalysis.py
+++ Losers_StockMarketAnalysis.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from IndividualComapnyAnalysis_StockMarketAnalysis import IndividualCompanyAnalysis
 
 URL = f"https://www.google.com/finance/markets/losers"
 
@@ -30,8 +31,22 @@ for money in LoserMarketValue[10: 10+len(LoserCompanyNames_List)]:
 for percentage in LoserPercentage[10: 10+len(LoserCompanyNames_List)]:
     LoserPercentage_List.append(percentage.text)
 
-
 Losers_DataFrame = pd.DataFrame({"Gainer Company Names": LoserCompanyNames_List, "Market Value": LoserMarketValue_List, "Increase Percentage": LoserPercentage_List}, index = np.arange(1, len(LoserCompanyNames_List)+1))
 
+print()
 print(Losers_DataFrame)
+print()
+
+while True:
+    CompanyDetails = input("Do you want to look into any Company's stock details? (y/n): ")
+
+    if CompanyDetails.lower() == 'y' or CompanyDetails.lower() == 'yes':
+        IndividualCompanyAnalysis()
+    elif CompanyDetails.lower() == 'n' or CompanyDetails.lower() == 'no':
+        print()
+        break
+    else:
+        print()
+        print("Invalid Input")
+        print()
 
