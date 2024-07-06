8156c564c6a17d1271c0c510af9d33b410b2c1bd
prnvvv <prannavakhanth12@gmail.com>
2024-07-06 19:11:47 +0530

    Update MostActive_StockMarketAnalysis.py

--- MostActive_StockMarketAnalysis.py
+++ MostActive_StockMarketAnalysis.py
@@ -21,7 +21,7 @@ MostActivePercentages = soup.find_all("div", class_ = "JwB6zf")
 MostActiveCompanyNames_List = []
 MostActiveMarketValues_List = []
 MostActivePercentages_List = []
-MostActiveCompanyChange_List = []
+MostActiveCompanyChanges_List = []
 
 
 for name in MostActiveCompanyNames:
@@ -35,14 +35,14 @@ for percentage in MostActivePercentages[10 : 10 + len(MostActiveCompanyNames_Lis
 
 for colour in MostActiveCompanyChanges:
     if colour["d"] ==  "M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z":
-        MostActiveCompanyChange_List.append("Down By ")
+        MostActiveCompanyChanges_List.append("Down By ")
     elif colour["d"] == "M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z":
-        MostActiveCompanyChange_List.append("Up By ")
+        MostActiveCompanyChanges_List.append("Up By ")
 
-MostActiveCompanyChange_List = MostActiveCompanyChange_List[10: 10 + len(MostActiveCompanyNames_List)]
+MostActiveCompanyChanges_List = MostActiveCompanyChanges_List[10: 10 + len(MostActiveCompanyNames_List)]
 
 for i in range(len(MostActiveCompanyNames_List)):
-    MostActivePercentages_List[i] = MostActiveCompanyChange_List[i] + MostActivePercentages_List[i]
+    MostActivePercentages_List[i] = MostActiveCompanyChanges_List[i] + MostActivePercentages_List[i]
 
 MostActive_DataFrame = pd.DataFrame({"Most Active Company Names": MostActiveCompanyNames_List, "Market Value": MostActiveMarketValues_List, "Change Percentage": MostActivePercentages_List}, index = np.arange(1, len(MostActiveCompanyNames_List)+1))
 
