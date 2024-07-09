0312c6460e7b7e19395a5d5ddf43ed0ef1f25587
prnvvv <prannavakhanth12@gmail.com>
2024-07-09 19:35:00 +0530

    Updated

--- MostFollowedOnGoogle_StockMarketAnalysis.py
+++ MostFollowedOnGoogle_StockMarketAnalysis.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import requests 
 from bs4 import BeautifulSoup
-from IndividualComapnyAnalysis_StockMarketAnalysis import IndividualCompanyAnalysis
 
 def MostFollowedOnGoogle():
     URL = f"https://www.google.com/finance/"
