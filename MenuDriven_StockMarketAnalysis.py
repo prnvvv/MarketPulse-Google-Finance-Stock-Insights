9d0335112330b5fe3e4e4e4d59fc16c769bac1e9
prnvvv <prannavakhanth12@gmail.com>
2024-07-11 00:45:17 +0530

    Update MenuDriven_StockMarketAnalysis.py

--- MenuDriven_StockMarketAnalysis.py
+++ MenuDriven_StockMarketAnalysis.py
@@ -4,6 +4,16 @@ from MostFollowedOnGoogle_StockMarketAnalysis import MostFollowedOnGoogle
 print()
 print("STOCK ANALYSIS WITH GOOGLE FINANCE")
 print()
+
+print()
 print("MOST FOLLOWED COMPANIES ON GOOGLE")
 print()
 MostFollowedOnGoogle()
+print()
+
+print()
+print("MENU")
+print()
+print("1. GAINERS")
+print("2. LOSERS")
+print("3. MOST ACTIVE")
