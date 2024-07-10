ab48ab29af8c1ce3613521b9bc76ffb88c64498f
prnvvv <prannavakhanth12@gmail.com>
2024-07-11 01:04:18 +0530

    Update MenuDriven_StockMarketAnalysis.py

--- MenuDriven_StockMarketAnalysis.py
+++ MenuDriven_StockMarketAnalysis.py
@@ -32,61 +32,62 @@ def main():
     except ValueError:
         print("Invalid input. Please enter a number between 1 and 4.")
         return
+    
+    while True:
+        if option == 1:
+            print("\nGAINERS\n")
+            Gainers_Dataframe = Gainers()
+            print(Gainers_Dataframe)
+            while True:
+                user_input = input("Do you want to look into any company's stock details? (y/n): ")
+                if user_input.lower() == 'y':
+                    IndividualCompanyAnalysis()
+                elif user_input.lower() == 'n':
+                    break
+                else:
+                    print("Invalid input. Please enter 'y' or 'n'.")
 
-    if option == 1:
-        print("\nGAINERS\n")
-        Gainers_Dataframe = Gainers()
-        print(Gainers_Dataframe)
-        while True:
-            user_input = input("Do you want to look into any company's stock details? (y/n): ")
-            if user_input.lower() == 'y':
-                IndividualCompanyAnalysis()
-            elif user_input.lower() == 'n':
-                break
-            else:
-                print("Invalid input. Please enter 'y' or 'n'.")
+        elif option == 2:
+            print("\nLOSERS\n")
+            Losers_Dataframe = Losers()
+            print(Losers_Dataframe)
+            while True:
+                user_input = input("Do you want to look into any company's stock details? (y/n): ")
+                if user_input.lower() == 'y':
+                    IndividualCompanyAnalysis()
+                elif user_input.lower() == 'n':
+                    break
+                else:
+                    print("Invalid input. Please enter 'y' or 'n'.")
 
-    elif option == 2:
-        print("\nLOSERS\n")
-        Losers_Dataframe = Losers()
-        print(Losers_Dataframe)
-        while True:
-            user_input = input("Do you want to look into any company's stock details? (y/n): ")
-            if user_input.lower() == 'y':
-                IndividualCompanyAnalysis()
-            elif user_input.lower() == 'n':
-                break
-            else:
-                print("Invalid input. Please enter 'y' or 'n'.")
+        elif option == 3:
+            print("\nMOST ACTIVE\n")
+            MostActive_Dataframe = MostActive()
+            print(MostActive_Dataframe)
+            while True:
+                user_input = input("Do you want to look into any company's stock details? (y/n): ")
+                if user_input.lower() == 'y':
+                    IndividualCompanyAnalysis()
+                elif user_input.lower() == 'n':
+                    break
+                else:
+                    print("Invalid input. Please enter 'y' or 'n'.")
 
-    elif option == 3:
-        print("\nMOST ACTIVE\n")
-        MostActive_Dataframe = MostActive()
-        print(MostActive_Dataframe)
-        while True:
-            user_input = input("Do you want to look into any company's stock details? (y/n): ")
-            if user_input.lower() == 'y':
-                IndividualCompanyAnalysis()
-            elif user_input.lower() == 'n':
-                break
-            else:
-                print("Invalid input. Please enter 'y' or 'n'.")
+        elif option == 4:
+            print("\nFAMOUS COMPANIES ON GOOGLE\n")
+            FamousCompanies_Dataframe = FamousCompanies()
+            print(FamousCompanies_Dataframe)
+            while True:
+                user_input = input("Do you want to look into any company's stock details? (y/n): ")
+                if user_input.lower() == 'y':
+                    IndividualCompanyAnalysis()
+                elif user_input.lower() == 'n':
+                    break
+                else:
+                    print("Invalid input. Please enter 'y' or 'n'.")
 
-    elif option == 4:
-        print("\nFAMOUS COMPANIES ON GOOGLE\n")
-        FamousCompanies_Dataframe = FamousCompanies()
-        print(FamousCompanies_Dataframe)
-        while True:
-            user_input = input("Do you want to look into any company's stock details? (y/n): ")
-            if user_input.lower() == 'y':
-                IndividualCompanyAnalysis()
-            elif user_input.lower() == 'n':
-                break
-            else:
-                print("Invalid input. Please enter 'y' or 'n'.")
-
-    else:
-        print("Invalid option. Please enter a number between 1 and 4.")
+        else:
+            print("Invalid option. Please enter a number between 1 and 4.")
 
 
 if __name__ == "__main__":
