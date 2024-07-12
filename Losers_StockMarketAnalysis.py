50cd3ed64ebdaf7143b57c8708e6125a7378b8c2
prnvvv <prannavakhanth12@gmail.com>
2024-07-12 13:53:32 +0530

    Update Losers_StockMarketAnalysis.py

--- Losers_StockMarketAnalysis.py
+++ Losers_StockMarketAnalysis.py
@@ -14,41 +14,47 @@ def Losers():
 
     soup = BeautifulSoup(response.text, "lxml")
 
+    # Extracting data from specific classes in the HTML
     LoserCompanyNames = soup.find_all("div", class_ = "ZvmM7")
     LoserMarketValues = soup.find_all("div", class_ = "YMlKec")
     LoserPercentages = soup.find_all("div", class_ = "JwB6zf")
     LoserCompanyCodes = soup.find_all("div", class_ = "COaKTb")
 
+    # Lists to store extracted data
     LoserCompanyNames_List = []
     LoserMarketValues_List = []
     LoserPercentages_List = []
     LoserCompanyCodes_List = []
 
-
+    # Extracting loser company names
     for name in LoserCompanyNames:
         LoserCompanyNames_List.append(name.text)
 
+    # Extracting loser market values
     for money in LoserMarketValues[10: 10+len(LoserCompanyNames_List)]:
         LoserMarketValues_List.append(money.text)
 
+    # Extracting loser percentage changes
     for percentage in LoserPercentages[10: 10+len(LoserCompanyNames_List)]:
         LoserPercentages_List.append(percentage.text)
 
+    # Extracting loser company codes
     for codename in LoserCompanyCodes:
         LoserCompanyCodes_List.append(codename.text)
 
+    # Trimming loser company codes list to desired length
     LoserCompanyCodes_List = LoserCompanyCodes_List[:50]
 
-    GainerCompanyCodes_List = LoserCompanyCodes_List[:50]
-
+    # Formatting percentage changes
     for i in range(len(LoserCompanyNames_List)):
         LoserPercentages_List[i] = f"Down by {LoserPercentages_List[i]}"
 
+    # Creating a DataFrame with extracted data
     Losers_DataFrame = pd.DataFrame({"Company Code": LoserCompanyCodes_List, "Company Name": LoserCompanyNames_List, "Market Value": LoserMarketValues_List, "Change Percentage": LoserPercentages_List}, index = np.arange(1, len(LoserCompanyNames_List)+1))
 
+    # Printing the DataFrame
     print()
     print("LOSERS")
     print()
     print(Losers_DataFrame)
     print()
-
